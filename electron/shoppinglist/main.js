73b3810edbc4253787ee801fbf2574117c54974d
Phillip Givens <psgivens11@yahoo.com>
2018-03-11 23:20:17 -0700

    Added electron shopping cart example.

---
+++ electron/shoppinglist/main.js
@@ -0,0 +1,98 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+process.env.NODE_ENV = 'production';
+let mainWindow;
+let addWindow;
+
+app.on('ready', function (){    
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWindow.on('closed', function(){
+        app.quit();
+    })
+
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add Shopping List Item'
+    });
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+}
+
+// Catch item:add
+ipcMain.on('item:add', function(e, item){
+    mainWindow.webContents.send('item:add', item);
+    addWindow.close();
+});
+
+const mainMenuTemplate = [
+    {
+        label: '&File',
+        submenu:[
+            {
+                label: 'Add Item',
+                click(){
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Clear Items',
+                click(){
+                    mainWindow.webContents.send('item:clear');
+                }
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }             
+        ]
+    }
+];
+
+// If mac, add empty object to menu
+if (process.platform == 'darwin'){
+    mainMenuTemplate.unshift({});
+}
+
+// Add developer tools item if not in prod
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    })
+    
+}
