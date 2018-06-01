f99f28bf885e30ab6ef8095b6e4f97120effbe12
Phillip Scott Givens <psgivens11@yahoo.com>
2018-06-01 07:07:31 -0700

    Adding line for installing powershell on 16.04

--- Desktop/.setup/install.sh
+++ Desktop/.setup/install.sh
@@ -52,10 +52,16 @@ sudo apt-get install -y sqlite3 libsqlite3-dev
 
 # Import the public repository GPG keys
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
 # Register the Microsoft Ubuntu repository
-curl https://packages.microsoft.com/config/ubuntu/17.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
+# Uncomment one of the following
+    #curl https://packages.microsoft.com/config/ubuntu/17.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
+
+    curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
+
 # Update the list of products
 sudo apt-get update
+
 # Install PowerShell
 sudo apt-get install -y powershell
 
