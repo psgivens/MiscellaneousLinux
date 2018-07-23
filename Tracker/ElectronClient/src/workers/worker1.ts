eb4644d688c3a2ffed0afb73c2734fae60c608bc
Phillip Scott Givens <psgivens11@yahoo.com>
2018-07-23 07:00:13 -0700

    Creating, adding to, and retrieving from, indexeddb.

--- Tracker/ElectronClient/src/workers/worker1.ts
+++ Tracker/ElectronClient/src/workers/worker1.ts
@@ -1,70 +1,83 @@
 onmessage = function(e) {
     console.log('Message received from main script');
     var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
-    const DBOpenRequest = this.indexedDB.open("toDoList")
+    const DBOpenRequest = this.indexedDB.open("Pomodoro", 4)
     
     let objectStore
     DBOpenRequest.onupgradeneeded = function(e) {
       let db = DBOpenRequest.result;
-      objectStore = db.createObjectStore("toDoList", { autoIncrement: true });
-      console.log("Successfully upgraded db")
-      console.log("Successfully upgraded db")
-      console.log("Successfully upgraded db")
-      console.log("Successfully upgraded db")
+      db.deleteObjectStore("Pomodoros")
+      objectStore = db.createObjectStore("Pomodoros", { keyPath: "id", autoIncrement: true });
+      objectStore.createIndex("idIdx", "id", { unique: true })
+      objectStore.createIndex("startIdx", "startTime", { unique: false })
+      objectStore.createIndex("userIdIdx", "userId", { unique: false })
+      console.log("**DB** Successfully upgraded db")
     };
 
 
     DBOpenRequest.onerror = function(event) {
-      console.log("onerror")
-      console.log("onerror")
-      console.log("onerror")
-      console.log("onerror")
-      console.log("onerror")
-      console.log(event)
+      console.log("**DB** DBRequest.onerror")
+      console.log(JSON.stringify(event))
     };
   
     let db;
     DBOpenRequest.onsuccess = function(event) {
-      console.log("onsuccess")
-      console.log("onsuccess")
-      console.log("onsuccess")
-      console.log("onsuccess")
-      console.log("onsuccess")
-      console.log(event)
+      console.log("**DB** DBRequest.onsuccess")
+      console.log(JSON.stringify(event))
   
       // store the result of opening the database in the db variable. This is used a lot below
       db = DBOpenRequest.result;
   
       // Run the displayData() function to populate the task list with all the to-do list data already in the IDB
-      const transaction = db.transaction(["toDoList"], "readwrite");
+      const transaction = db.transaction(["Pomodoros"], "readwrite");
       transaction.oncomplete = function() {
-        console.log("oncomplete")
-        console.log("oncomplete")
-        console.log("oncomplete")
-        console.log("oncomplete")
-        console.log("oncomplete")
-        console.log("oncomplete")
-        console.log("oncomplete")
+        console.log("**DB** transaction.oncomplete")
       };
 
-      objectStore = transaction.objectStore("toDoList");
-      console.log(objectStore.indexNames);
-      console.log(objectStore.keyPath);
-      console.log(objectStore.name);
-      console.log(objectStore.transaction);
-      console.log(objectStore.autoIncrement);
+      objectStore = transaction.objectStore("Pomodoros");
+      console.log(JSON.stringify(objectStore.indexNames))
+      console.log(JSON.stringify(objectStore.keyPath))
+      console.log(JSON.stringify(objectStore.name))
+      console.log(JSON.stringify(objectStore.transaction))
+      console.log(JSON.stringify(objectStore.autoIncrement))
 
-
-
-      const objectStoreRequest = objectStore.add({name: "bob" });
+      const objectStoreRequest = objectStore.add({name: "rob", userId: "bsmith", startTime: new Date("2015-03-25T12:00:00Z") });
       objectStoreRequest.onsuccess = function() {
-          console.log("onsucces")
-          console.log("onsucces")
-          console.log("onsucces")
-          console.log("onsucces")
-          console.log("onsucces")
+          console.log("**DB** OSRequest.onsucces")
+      };      
+      const index = db.transaction("Pomodoros").objectStore("Pomodoros").index("startIdx")
+      var boundKeyRange = IDBKeyRange.bound(new Date("2010-03-25T12:00:00Z"), new Date("2018-03-25T12:00:00Z"), false, true);
+
+      index.openCursor(boundKeyRange).onsuccess = function(event:any) {
+        var cursor = event.target.result;
+        if (cursor) {
+          console.log(JSON.stringify(cursor.value))
+          cursor.continue();
+        }
+        else {
+          console.log("Got all records ");
+        }
       };
+
+      const index2 = db.transaction("Pomodoros").objectStore("Pomodoros").index("userIdIdx")
+      var boundKeyRange2 = IDBKeyRange.bound("andy", "zed", false, true);
+
+      index.openCursor(boundKeyRange).onsuccess = function(event:any) {
+        var cursor = event.target.result;
+        if (cursor) {
+          console.log(JSON.stringify(cursor.value))
+          cursor.continue();
+        }
+        else {
+          console.log("Got all records ");
+        }
+      };
+
     };
+
+    
+
+
     console.log('Posting message back to main script');
     postMessage(workerResult, "worker1");
 }
