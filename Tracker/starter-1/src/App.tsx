4a4e579382d346a3819000e0759d364540460cb4
Phillip Scott Givens <psgivens11@yahoo.com>
2018-08-08 19:10:59 -0700

    Adding packages from https://hackernoon.com/building-a-website-with-react-and-bulma-d655214bff2a to get bulma working.

--- Tracker/starter-1/src/App.tsx
+++ Tracker/starter-1/src/App.tsx
@@ -12,9 +12,53 @@ type BasicProps = {} & {}
 const App: React.SFC<BasicProps> = () => 
     (<Router>
         <>
+        <aside className="menu">
+            <p className="menu-label">
+                General
+            </p>
+            <ul className="menu-list">
+                <li><a>Dashboard</a></li>
+                <li><a>Customers</a></li>
+            </ul>
+            <p className="menu-label">
+                Administration
+            </p>
+            <ul className="menu-list">
+                <li><a>Team Settings</a></li>
+                <li>
+                <a className="is-active">Manage Your Team</a>
+                <ul>
+                    <li><a>Members</a></li>
+                    <li><a>Plugins</a></li>
+                    <li><a>Add a member</a></li>
+                </ul>
+                </li>
+                <li><a>Invitations</a></li>
+                <li><a>Cloud Storage Environment Settings</a></li>
+                <li><a>Authentication</a></li>
+            </ul>
+            <p className="menu-label">
+                Transactions
+            </p>
+            <ul className="menu-list">
+                <li><a>Payments</a></li>
+                <li><a>Transfers</a></li>
+                <li><a>Balance</a></li>
+            </ul>
+          </aside>
           <Route path="/" component={ SandBox } />
+          <section className="section">
+            <div className="container">
+            <h1 className="title">
+                Hello World
+            </h1>
+            <p className="subtitle">
+                My first website with <strong>Bulma</strong>!
+            </p>
+            </div>
+        </section>
           Hello World
-          <ul>
+          <ul >
               <li><Link to="/">Home</Link> </li>
           </ul>
           
