e93b8c5c22059e3a69dfedb9e69373c9cc2a93d5
Phillip Scott Givens <psgivens11@yahoo.com>
2018-08-27 21:36:14 -0700

    Creating a script to try the saas identity walkthrough.

---
+++ saasid/SaasIdentity.ps1
@@ -0,0 +1,57 @@
+
+# Import the AWS modules
+Import-Module AWSPowerShell.NetCore
+
+# Set the credentials from your account
+Set-AWSCredentials 
+  -AccessKey '<accesskey>' `
+  -SecretKey '<secretkey>' `
+  -StoreAs 'profile_name'
+
+# Set to the current profile
+Set-AWSCredentials -ProfileName "pp-psg"
+
+# Should print the same profile name as above.
+Write-Host "Current profile is: $StoredAWSCredentials"
+
+# Set the default region
+Set-DefaultAWSRegion -Region "us-east-2"
+
+# Create a keypair and assign it to a variable. 
+$pair = New-EC2KeyPair -KeyName SaasIdentities
+
+# Move to the ~/.keys directory and write the pem file
+pushd ~/.keys
+$pair.KeyMaterial >> SaasIdentities.pem
+chmod 400 SaasIdentities.pem
+
+popd 
+
+$pair.KeyFingerPrint >> SaasIdentities.finger
+
+
+$cfn_template = "https://aws-quickstart.s3.amazonaws.com/saas-identity-cognito/templates/saas-identity-cognito-master.template"
+
+# Let's take a look at the template
+Test-CFNTemplate -TemplateURL $cfn_template `
+  | %{ $_.Parameters } `
+  | Select -Property ParameterKey, DefaultValue, Description
+
+Test-CFNTemplate -TemplateURL $cfn_template | Select -ExpandProperty Description
+
+$params = cat ./parameters.json | ConvertFrom-Json
+$params
+#
+$cfnarn = New-CFNStack `
+  -TemplateUrl $cfn_template `
+  -Stackname 'SaaS-identity-with-Cognito' `
+  -Parameter $params
+$cfnarn | Tee-Object -FilePath 'cfn.arn'
+
+
+# arn created during this process.
+
+
+
+
+ 
