dc2b54aba607395d2e6c40fee5d01ffa94ba06f6
Phillip Scott Givens <psgivens11@yahoo.com>
2019-01-06 11:37:36 -0800

    stuff

---
+++ Desktop/.setup/bootstrap.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Bootstrapping the environment
+
+### Install tools
+
+    sudo apt update
+    sudo apt install -y git
+    sudo apt install -y curl
+    sudo apt install -y vim
+    sudo apt install -y tmux
+
+### Git this repo
+
+    mkdir -p ~/Repos/psgivens
+    cd ~/Repos/psgivens
+    git clone https://github.com/psgivens/MiscellaneousLinux.git misc.git
+    cd misc.git/Desktop
+
+### Symbolic link config files
+   
+    ln -s "$(pwd)/.vimrc" ~/.vimrc
+    ln -s "$(pwd)/.tmux.conf" ~/.tmux.conf
+
+    
+### Configure vim for pathogen
+
+    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+### Install vim environment
+
+    git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+    git clone https://github.com/jpalardy/vim-slime.git ~/.vim/bundle/vim-slime
+    git clone https://github.com/mattn/emmet-vim.git ~/.vim/bundle/emmet-vim
+    
+    
+### Use slime to install rest
+    
+    cd .setup
+    tmux 
+    
+    
+    
+    
+    
